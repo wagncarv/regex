fa768682f3a712284defeabadbe1abdfe27955c4
Wagner Carvalho <wagncarv@gmail.com>
2021-02-09 15:24:08 -0300

    changes

--- Flags.js
+++ Flags.js
@@ -1,8 +1,12 @@
 // g - global
 // i - ignore case
 
-const texto = 'Carlos assinou o abaixo-assinado'
-console.log(texto.match(/C|ab/))
-console.log(texto.match(/c|ab/i))
-console.log(texto.match(/ab|c/gi))
-console.log(texto.match(/ab|c/i))
+// const texto = 'Carlos assinou o abaixo-assinado'
+// console.log(texto.match(/C|ab/))
+// console.log(texto.match(/c|ab/i))
+// console.log(texto.match(/ab|c/gi))
+// console.log(texto.match(/ab|c/i))
+
+const texto = 'Casa bonita é casa amarela da esquina com a Rua ACASALAR.'
+const regex = /a c/gi
+console.log(texto.match(regex))
