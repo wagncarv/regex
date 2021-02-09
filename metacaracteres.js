c8049bfc0481660d18b47c3c5cd635d3a916086f
Wagner Carvalho <wagncarv@gmail.com>
2021-02-09 17:07:35 -0300

    metecaracteres simples

---
+++ metacaracteres.js
@@ -0,0 +1,18 @@
+
+//   metacaractere    Nome                   Significado
+//    .               Ponto                   qualquer caractere
+//    []              Conjunto                conjunto de caracteres permitidos
+//    [^]             Conjunto negado         conjunto de caracteres proibidos
+
+//Metacaracteres - quantificadores
+// ?  Opcional  zero ou um
+// *  asterisco  zero ou mais
+// +  mais  um ou mais
+//  {n, m} chaves  de n até m
+
+// Metacaracteres - Âncoras
+// ^  início de linha
+// $  cifrão  fim de linha
+// \b  borda início ou fim de palavra
+
+
