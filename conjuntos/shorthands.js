fdfca4a7c1aaf066979658b9f0692c1fef3783e5
Wagner Carvalho <wagncarv@gmail.com>
2021-02-11 11:49:05 -0300

    conjuntos

--- conjuntos/shorthands.js
+++ conjuntos/shorthands.js
@@ -1,2 +1,9 @@
-﻿const texto = `
-`
+﻿const texto = `1,2,3,4,5,6,a.b c!d?e    -
+f_g`
+
+console.log(texto.match(/\d/g)) //Números [0-9]
+console.log(texto.match(/\D/g)) //Não números [^0-9]
+console.log(texto.match(/\w/g)) //Caracteres [a-zA-Z0-9]
+console.log(texto.match(/\W/g)) //não caracteres [a-zA-Z]
+console.log(texto.match(/\s/g)) //espaço
+console.log(texto.match(/\S/g)) //não espaço
